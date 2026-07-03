33e5ef359c51beb982712585e204695048c10558
Evgenii Maslov <maslov@airi.net>
2026-07-03 17:38:30 +0300

    fix: clean legacy

--- experiments/benchmarks/coma/results_visualization/scripts/multimodal_line_visualization.sh
+++
@@ -1,3 +0,0 @@
-HYDRA_FULL_ERROR=1 python /mnt/virtual_ai0001071-04017_SR004-nfs1/CFS-SR008/workspace/maslov/massing_generation/hydra_run.py \
---config_path /mnt/virtual_ai0001071-04017_SR004-nfs1/CFS-SR008/workspace/maslov/massing_generation/experiments/benchmarks/coma/results_visualization/visualization_configs/multimodal_line_visualization.yaml \
---multirun x_feature=context_1d_count,context_2d_count,context_3d_count
--- experiments/training/coma/291125_alternative_framework/README.md
+++
@@ -1,2 +0,0 @@
-Here I want to use native transformers-based training pipeline, provided from Qwen team, to compare convergence with my framework.
-First run prepare_data.py, then train.sh.
